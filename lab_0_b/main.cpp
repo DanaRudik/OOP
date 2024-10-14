8d8ef6cac44ea5a51b21c25a5cc530bf1ef06625
DanaRudik <danarudik462@gmail.com>
2024-10-14 18:56:21 +0700

    build resources

--- lab_0_b/main.cpp
+++ lab_0_b/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(int, char**)
 {
-    ifstream file("a.txt", ios_base::in); 
+    ifstream file("resources/a.txt", ios_base::in); 
     for(string i; getline(file,i);)
     {
         models::entity af(i);
     };
-    cout << "Hello, from lab0b!\n";
+    file.close();
 }
